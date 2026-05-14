fe23c86d25f0973b84c3191cab4e224cd4337128
Alexander Hoinville <ahoin001@gmail.com>
2026-05-14 14:58:53 -0400

    release: v1.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.darusc.vcamdroid"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.3.2"
+        versionCode = 3
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
